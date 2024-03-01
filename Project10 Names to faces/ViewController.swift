f9bcb5468c4e52f721da335d57ed78711f9db035
Lydia <guipopo5@gmail.com>
2024-03-01 21:29:53 +0800

    Initial Commit

---
+++ Project10 Names to faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10 Names to faces
+//
+//  Created by Lydia Lu on 2024/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
